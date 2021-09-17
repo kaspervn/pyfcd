f7a806d21aaefc75f9892ce1e3961377593fb1a2
kasper <kasper.dev@fastmail.com>
2021-09-17 14:18:59 +0200

    Added acknowledgement to fft_inverse_gradient.py

--- fft_inverse_gradient.py
+++ fft_inverse_gradient.py
@@ -2,7 +2,8 @@ import numpy as np
 from numpy.fft import fftfreq
 from scipy.fft import fft2, ifft2
 
-#TODO: acknowledge original author
+# Based on work from Sander Wildeman, which based it on the following paper:
+#   Huhn, et al. Exp Fluids (2016), 57, 151, https://doi.org/10.1007/s00348-016-2236-3
 
 def fftinvgrad(fx, fy):
     size = fx.shape
